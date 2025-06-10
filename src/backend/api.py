db6d7ec2ecda458513fd400d1cd8ff040f9f22dc
JENSON Chang <jensonchang@gmail.com>
2025-06-09 22:14:13 -0700

    Update api.py

--- src/backend/api.py
+++ src/backend/api.py
@@ -340,7 +340,7 @@ def search():
         if query_text:
             if os.getenv("GOOGLE_API_KEY") or os.getenv("OPENAI_API_KEY"):
                 reordered_result, reasoning = reorder_search_results_by_relevancy(
-                    query_text, formatted_result, max_results=0.3*top_k
+                    query_text, formatted_result, max_results=int(0.3*top_k)
                 )
             else:
                 reordered_result = formatted_result[:]
